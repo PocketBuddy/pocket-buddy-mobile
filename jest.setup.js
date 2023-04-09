3da5db2fb30235e4a6043e9fee3b36287d76c7b9
kamil.buczynski <kamil.buczynski@stxnext.pl>
2023-04-09 22:40:47 +0200

    test: mock RNLocalize

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,9 @@
+import mockRNLocalize from 'react-native-localize/mock';
 import 'whatwg-fetch';
 import 'react-native-gesture-handler/jestSetup';
 import '@testing-library/jest-native/extend-expect';
+
+jest.mock('react-native-localize', () => mockRNLocalize);
 
 jest.mock('react-native-reanimated', () =>
   require('react-native-reanimated/mock'),
