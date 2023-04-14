144eb7a11ea2807a6005fd6905e664d7a5e6b4e1
kamil.buczynski <kamil.buczynski@stxnext.pl>
2023-04-14 19:41:11 +0200

    fix(auth): fix validation schema for register

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -5,29 +5,30 @@ const MIN_PASSWORD_LENGTH = 8;
 
 const login = yup.object({
   name: yup.string().required(i18n.t('schemas:auth.name.required')),
-  password: yup
-    .string()
-    .min(
-      MIN_PASSWORD_LENGTH,
-      i18n.t('schemas:auth.password.minLength', {
-        minLength: MIN_PASSWORD_LENGTH,
-      }),
-    )
-    .required(i18n.t('schemas:auth.password.required')),
+  password: yup.string().required(i18n.t('schemas:auth.password.required')),
 });
 
 const passwordRecovery = yup.object({
   email: yup
     .string()
-    .email(i18n.t('schemas:auth.email.valid'))
-    .required(i18n.t('schemas:auth.email.required')),
+    .required(i18n.t('schemas:auth.email.required'))
+    .email(i18n.t('schemas:auth.email.valid')),
 });
 
 const register = login.concat(passwordRecovery).shape({
+  password: yup
+    .string()
+    .required(i18n.t('schemas:auth.password.required'))
+    .min(
+      MIN_PASSWORD_LENGTH,
+      i18n.t('schemas:auth.password.minLength', {
+        minLength: MIN_PASSWORD_LENGTH,
+      }),
+    ),
   confirmPassword: yup
     .string()
-    .oneOf([yup.ref('password')], i18n.t('schemas:auth.confirmPassword.match'))
-    .required(i18n.t('schemas:auth.confirmPassword.required')),
+    .required(i18n.t('schemas:auth.confirmPassword.required'))
+    .oneOf([yup.ref('password')], i18n.t('schemas:auth.confirmPassword.match')),
 });
 
 export default {
