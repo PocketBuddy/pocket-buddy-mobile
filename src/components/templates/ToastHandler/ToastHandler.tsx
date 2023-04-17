5509ca2ed73f973ed8696e81a00ade1e30c89186
kamil.buczynski <kamil.buczynski@stxnext.pl>
2023-04-17 21:09:28 +0200

    fix(ToastHandler): set top offset for Android

--- src/components/templates/ToastHandler/ToastHandler.tsx
+++ src/components/templates/ToastHandler/ToastHandler.tsx
@@ -1,10 +1,11 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useMemo } from 'react';
 import Toast, { BaseToastProps } from 'react-native-toast-message';
 import { useDispatch, useSelector } from 'react-redux';
 import { hide } from '@/store/toast';
 import { RootState } from '@/store';
 import { ToastMessage } from '@/components';
 import { ToastType } from 'types/components';
+import { usePlatform } from '@/hooks';
 
 const toastConfig = {
   success: (props: BaseToastProps) => (
@@ -20,12 +21,15 @@ const toastConfig = {
 
 export default function ToastError() {
   const dispatch = useDispatch();
+  const { isAndroid } = usePlatform();
   const isOpen = useSelector((state: RootState) => state.toast.isOpen);
   const header = useSelector((state: RootState) => state.toast.header);
   const message = useSelector((state: RootState) => state.toast.message);
   const type = useSelector((state: RootState) => state.toast.type);
 
   const handleClose = useCallback(() => dispatch(hide()), []);
+
+  const topOffset = useMemo(() => (isAndroid ? 0 : undefined), [isAndroid]);
 
   useEffect(() => {
     if (!isOpen) {
@@ -38,5 +42,7 @@ export default function ToastError() {
     });
   }, [isOpen, header, message, type]);
 
-  return <Toast config={toastConfig} onHide={handleClose} />;
+  return (
+    <Toast config={toastConfig} onHide={handleClose} topOffset={topOffset} />
+  );
 }
