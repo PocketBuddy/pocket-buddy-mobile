12cb5a33c103b3687d17c1284c779dca40c401d6
kamil.buczynski <kamil.buczynski@stxnext.pl>
2023-06-02 12:45:48 +0200

    test(transactions): add tests for transactions store

--- src/store/transactions/index.ts
+++ src/store/transactions/index.ts
@@ -49,7 +49,7 @@ const transactionListSorter = (
   return dateB.getTime() - dateA.getTime();
 };
 
-const slice = createSlice({
+export const transactionsSlice = createSlice({
   name: 'transactions',
   initialState,
   reducers: {
@@ -75,19 +75,37 @@ const slice = createSlice({
           );
           if (transactionsGroupIndex !== -1) {
             let expenseUpdatedIndex = -1;
+
             const expenseIndex = state.list[
               transactionsGroupIndex
             ].expenses.findIndex(savedExpense => {
-              transaction.expenses.forEach((expense, index) => {
+              for (const [index, expense] of transaction.expenses.entries()) {
                 if (expense.id === savedExpense.id) {
                   expenseUpdatedIndex = index;
-                  return true;
+                  break;
                 }
-              });
+              }
+              return expenseUpdatedIndex !== -1;
             });
             if (expenseIndex !== -1) {
-              state.list[transactionsGroupIndex].expenses[expenseIndex] =
-                transaction.expenses[expenseUpdatedIndex];
+              state.list[transactionsGroupIndex].expenses[expenseIndex] = {
+                ...state.list[transactionsGroupIndex].expenses[expenseIndex],
+                ...transaction.expenses[expenseUpdatedIndex],
+              };
+              state.list[transactionsGroupIndex].amount_sum = state.list[
+                transactionsGroupIndex
+              ].expenses.reduce((acc, expense) => acc + expense.amount, 0);
+            } else {
+              state.list[transactionsGroupIndex] = {
+                ...state.list[transactionsGroupIndex],
+                amount_sum:
+                  state.list[transactionsGroupIndex].amount_sum +
+                  transaction.amount_sum,
+                expenses: [
+                  ...state.list[transactionsGroupIndex].expenses,
+                  ...transaction.expenses,
+                ],
+              };
             }
           } else {
             state.list = [...state.list, transaction];
@@ -115,7 +133,7 @@ const slice = createSlice({
             ],
           };
         } else {
-          state.list.unshift({
+          state.list.push({
             amount_sum: payload.amount,
             spent_date,
             expenses: [payload],
@@ -126,40 +144,79 @@ const slice = createSlice({
     },
     editTransaction: (state, { payload }: EditTransactionPayload) => {
       if (payload !== undefined) {
-        const { spent_date } = payload;
-        const index = state.list.findIndex(
-          transaction => transaction.spent_date === spent_date,
+        const newDateIndex = state.list.findIndex(
+          transaction => transaction.spent_date === payload.spent_date,
         );
-        if (index !== -1) {
-          const transactionIndex = state.list[index].expenses.findIndex(
+
+        // if new date existed
+        if (newDateIndex !== -1) {
+          const transactionIndex = state.list[newDateIndex].expenses.findIndex(
             transaction => transaction.id === payload.id,
           );
-          if (transactionIndex !== -1) {
-            const dateChanged =
-              state.list[index].expenses[transactionIndex].spent_date !==
-              payload.spent_date;
 
-            if (dateChanged) {
-              state.list[index].expenses.splice(transactionIndex, 1);
-              const newIndex = state.list.findIndex(
-                transaction => transaction.spent_date === payload.spent_date,
+          // if date group is not changed
+          if (transactionIndex !== -1) {
+            state.list[newDateIndex].expenses[transactionIndex] = payload;
+            state.list[newDateIndex].amount_sum = state.list[
+              newDateIndex
+            ].expenses.reduce((acc, expense) => acc + expense.amount, 0);
+          } else {
+            const oldItem = state.list
+              .flatMap(transaction => transaction.expenses)
+              .find(transaction => transaction.id === payload.id);
+
+            if (oldItem) {
+              const oldDateIndex = state.list.findIndex(
+                transaction => transaction.spent_date === oldItem.spent_date,
               );
-              if (newIndex !== -1) {
-                state.list[newIndex].expenses.unshift(payload);
-                return;
-              } else {
-                state.list.unshift({
-                  amount_sum: payload.amount,
-                  spent_date: payload.spent_date,
-                  expenses: [payload],
-                });
-                state.list.sort(transactionListSorter);
-                return;
+
+              state.list[oldDateIndex].expenses = state.list[
+                oldDateIndex
+              ].expenses.filter(transaction => transaction.id !== payload.id);
+              state.list[oldDateIndex].amount_sum = state.list[
+                oldDateIndex
+              ].expenses.reduce((acc, expense) => acc + expense.amount, 0);
+
+              if (!state.list[oldDateIndex].expenses.length) {
+                state.list.splice(oldDateIndex, 1);
               }
             }
-            state.list[index].expenses[transactionIndex] = payload;
+
+            state.list[newDateIndex].expenses.push(payload);
+            state.list[newDateIndex].amount_sum += payload.amount;
           }
         }
+        // if we need to create new date group
+        else {
+          const oldItem = state.list
+            .flatMap(transaction => transaction.expenses)
+            .find(transaction => transaction.id === payload.id);
+
+          if (oldItem) {
+            const oldDateIndex = state.list.findIndex(
+              transaction => transaction.spent_date === oldItem.spent_date,
+            );
+
+            state.list[oldDateIndex].expenses = state.list[
+              oldDateIndex
+            ].expenses.filter(transaction => transaction.id !== payload.id);
+            state.list[oldDateIndex].amount_sum = state.list[
+              oldDateIndex
+            ].expenses.reduce((acc, expense) => acc + expense.amount, 0);
+
+            if (!state.list[oldDateIndex].expenses.length) {
+              state.list.splice(oldDateIndex, 1);
+            }
+          }
+
+          state.list.push({
+            amount_sum: payload.amount,
+            spent_date: payload.spent_date,
+            expenses: [payload],
+          });
+        }
+
+        state.list.sort(transactionListSorter);
       }
     },
     removeTransaction: (state, { payload }: DeleteTransactionPayload) => {
@@ -194,6 +251,6 @@ export const {
   editTransaction,
   removeTransaction,
   removeTransactions,
-} = slice.actions;
+} = transactionsSlice.actions;
 
-export default slice.reducer;
+export default transactionsSlice.reducer;
